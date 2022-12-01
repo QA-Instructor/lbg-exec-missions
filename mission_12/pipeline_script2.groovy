9a6d4038b0a2a50b5428b7d63dd57b27bb9bf56a
Victoria <VictoriaLloyd@hotmail.com>
2022-12-01 14:16:40 +0000

    Mission 12 pipeline script 2

--- mission_12/pipeline_script2.groovy
+++ mission_12/pipeline_script2.groovy
@@ -1,3 +1,4 @@
+// This adds a quality gate that aborts the pipeline if the quality threshold isn't met
 pipeline {
   agent any
 
@@ -15,8 +16,11 @@ pipeline {
         steps {
             withSonarQubeEnv('sonar-qube-1') {        
               sh "${scannerHome}/bin/sonar-scanner"
-            }   
+        }
+        timeout(time: 10, unit: 'MINUTES'){
+          waitForQualityGate abortPipeline: true
         }
     }
   }
+}
 }
