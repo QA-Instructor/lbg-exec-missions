c44107561f09b4951e959b22b0a1462887d6cf13
Victoria <VictoriaLloyd@hotmail.com>
2022-12-01 15:10:18 +0000

    Mission 14 pipeline script 4

---
+++ mission_14/pipeline_script4.groovy
@@ -0,0 +1,26 @@
+// This adds a quality gate that aborts the pipeline if the quality threshold isn't met
+pipeline {
+  agent any
+
+  stages {
+    stage('Checkout') {
+        steps {
+          // Get some code from a GitHub repository
+          git branch: 'main', url: 'https://github.com/QA-Instructor/lbg-python-flask-sonarqube.git'
+        }
+    }
+    stage('SonarQube Analysis') {
+      environment {
+        scannerHome = tool 'sonarqube'
+      }
+        steps {
+            withSonarQubeEnv('sonar-qube-1') {        
+              sh "${scannerHome}/bin/sonar-scanner"
+        }
+        timeout(time: 10, unit: 'MINUTES'){
+          waitForQualityGate abortPipeline: true
+        }
+    }
+  }
+}
+}
