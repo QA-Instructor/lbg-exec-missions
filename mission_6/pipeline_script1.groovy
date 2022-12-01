e7aa58fe606bb1296fe5294274a9b7dc1efe6aa1
Victoria <VictoriaLloyd@hotmail.com>
2022-12-01 18:23:33 +0000

    Mission 6

---
+++ mission_6/pipeline_script1.groovy
@@ -0,0 +1,24 @@
+pipeline {
+        agent any
+
+        tools {
+            // Install the Maven version configured as "M3" and add it to the path.
+            maven "M3"
+        }
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    // Get some code from a GitHub repository
+
+                    git branch: 'main', url: 'YOUR GITHUB REPO URL HERE'
+                }
+            }
+            stage('Compile') {
+                steps {
+                    // Run Maven on a Unix agent.
+                    sh "mvn clean compile"
+                }
+            }
+        }
+}
---
+++ mission_6/pipeline_script2.groovy
@@ -0,0 +1,28 @@
+pipeline {
+        agent any
+        tools {
+            // Install the Maven version configured as "M3" and add it to the path.
+            maven "M3"
+        }
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    // Get some code from a GitHub repository
+
+                    git branch: 'main', url: 'YOUR GITHUB REPO URL HERE'
+                }
+            }
+            stage('Compile') {
+                steps {
+                    // Run Maven on a Unix agent.
+                    sh "mvn clean compile"
+                }
+            }
+            stage('Test') {
+                steps {
+                    sh "mvn test"
+                }
+            }
+        }
+}
